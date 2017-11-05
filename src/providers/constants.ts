412c3813c8834d2cb8296c313daebf02f235c9dc
tomwey <tomwey@163.com>
2017-11-05 16:45:06 +0800

    ccc

--- src/providers/constants.ts
+++ src/providers/constants.ts
@@ -5,5 +5,5 @@ export class Constants {
   public static APP_VERSION = '1.0';
   public static APP_SERVER = '';
 
-  public static APP_TYPE: number = 2; // 1 表示有声小说, 2 表示追书小说
+  public static APP_TYPE: number = 1; // 1 表示有声小说, 2 表示追书小说
 }
