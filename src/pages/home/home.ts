9b7b900e2493abd4b8f83e5148e126974cd6099a
tomwey <tomwey@163.com>
2017-11-07 15:24:57 +0800

    ccc

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,14 +6,70 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  selectedMenu: string = '我的收藏';
+  
+  isDropdown: boolean = false;
 
   constructor(public navCtrl: NavController) {
-
+    
   }
 
   editOrDone() 
   {
     
   }
+
+  selectMenu(i): void {
+    this.selectedMenu = this.menus[i].label;
+  }
+
+  toggle(yesOrNo): void {
+    this.isDropdown = yesOrNo;
+  }
+
+  reorderItems(indexes): void {
+    let menu = this.menus[indexes.from];
+    this.menus.splice(indexes.from, 1);
+    this.menus.splice(indexes.to, 0, menu);
+  }
+
+  menus: any = [
+    {
+      id: 'favorites',
+      label: '我的收藏',
+      empty: '目前没有收藏',
+      data: [],
+    },
+    {
+      id: 'uploaded',
+      label: '电脑上传',
+      empty: '',
+      data: [],
+    },
+    {
+      id: 'histories',
+      label: '历史记录',
+      empty: '目前没有历史记录',
+      data: [],
+    },
+    {
+      id: 'downloaded',
+      label: '下载完成',
+      empty: '目前没有下载缓存',
+      data: [],
+    },
+    {
+      id: 'downloading',
+      label: '正在下载',
+      empty: '目前没有下载任务',
+      data: [],
+    },
+    {
+      id: 'bookmarks',
+      label: '我的书签',
+      empty: '目前没有书签',
+      data: [],
+    },
+  ];
 
 }
